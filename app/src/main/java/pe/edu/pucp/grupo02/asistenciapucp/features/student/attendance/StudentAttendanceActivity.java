000f27b275184215e3ea98d27c504a1af10a198a
Darklight777 <triforce_courage@hotmail.com>
2019-06-18 20:37:44 -0500

    New Activities

---
+++ app/src/main/java/pe/edu/pucp/grupo02/asistenciapucp/features/student/attendance/StudentAttendanceActivity.java
@@ -0,0 +1,15 @@
+package pe.edu.pucp.grupo02.asistenciapucp.features.student.attendance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pe.edu.pucp.grupo02.asistenciapucp.R;
+
+public class StudentAttendanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_attendance);
+    }
+}
---
+++ app/src/main/java/pe/edu/pucp/grupo02/asistenciapucp/features/teacher/attendance/TeacherAttendanceActivity.java
@@ -0,0 +1,15 @@
+package pe.edu.pucp.grupo02.asistenciapucp.features.teacher.attendance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pe.edu.pucp.grupo02.asistenciapucp.R;
+
+public class TeacherAttendanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_attendance);
+    }
+}

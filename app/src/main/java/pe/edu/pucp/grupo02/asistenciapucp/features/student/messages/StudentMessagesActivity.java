11f3a6af1f91fe8ac5c5350ca7f5e65c22eb9024
Stef15ortiz <ortizeng2000@hotmail.com>
2019-06-27 18:06:07 -0500

    Pantallas

--- app/src/main/java/pe/edu/pucp/grupo02/asistenciapucp/features/student/messages/StudentMessagesActivity.java
+++ app/src/main/java/pe/edu/pucp/grupo02/asistenciapucp/features/student/messages/StudentMessagesActivity.java
@@ -1,9 +1,12 @@
 package pe.edu.pucp.grupo02.asistenciapucp.features.student.messages;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import pe.edu.pucp.grupo02.asistenciapucp.R;
+import pe.edu.pucp.grupo02.asistenciapucp.features.student.StudentActivity;
 
 public class StudentMessagesActivity extends AppCompatActivity {
 
@@ -12,4 +15,7 @@ public class StudentMessagesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_messages);
     }
-}
+    public void retroceder(View view){
+        Intent anterior = new Intent(this, StudentActivity.class);
+        startActivity(anterior);
+}}

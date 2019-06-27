e244decccc08e7e47fabfaf93444548127e6baff
Stef15ortiz <ortizeng2000@hotmail.com>
2019-06-26 20:56:23 -0500

    Pantallas tipo glovo

--- app/src/main/java/pe/edu/pucp/grupo02/asistenciapucp/features/student/StudentActivity.java
+++ app/src/main/java/pe/edu/pucp/grupo02/asistenciapucp/features/student/StudentActivity.java
@@ -1,9 +1,12 @@
 package pe.edu.pucp.grupo02.asistenciapucp.features.student;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import pe.edu.pucp.grupo02.asistenciapucp.R;
+import pe.edu.pucp.grupo02.asistenciapucp.features.home.HomeActivity;
 
 public class StudentActivity extends AppCompatActivity {
 
@@ -11,5 +14,9 @@ public class StudentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
+    }
+    public void retroceder(View view){
+        Intent anterior = new Intent(this, HomeActivity.class);
+        startActivity(anterior);
     }
 }

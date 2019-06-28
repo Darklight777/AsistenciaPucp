3de98c4a9c7c74caa46f436125529741f75c160a
dispositivos123 <programacion.dispositivos@gmail.com>
2019-06-27 19:40:02 -0500

    course e

---
+++ app/src/main/java/pe/edu/pucp/grupo02/asistenciapucp/data/db/entities/Course.java
@@ -0,0 +1,17 @@
+package pe.edu.pucp.grupo02.asistenciapucp.data.db.entities;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+
+@Entity(tableName = "COURSE")
+public class Course {
+    @PrimaryKey
+    @ColumnInfo(name = "COURSE_ID")
+    private int courseId;
+
+    @ColumnInfo(name = "COURSE_NAME")
+    private String courseName;
+
+}

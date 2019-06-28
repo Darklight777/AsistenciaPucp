ba22a6c504bedc1b41b9dda1acd0638fa03dd8ed
Alejo0807 <31521198+Alejo0807@users.noreply.github.com>
2019-06-28 15:21:18 -0500

    Alejandro mock

--- app/src/main/java/pe/edu/pucp/grupo02/asistenciapucp/features/student/token/IStudentTokenPresenter.java
+++ app/src/main/java/pe/edu/pucp/grupo02/asistenciapucp/features/student/token/IStudentTokenPresenter.java
@@ -3,4 +3,8 @@ package pe.edu.pucp.grupo02.asistenciapucp.features.student.token;
 import pe.edu.pucp.grupo02.asistenciapucp.features.base.IPresenter;
 
 public interface IStudentTokenPresenter extends IPresenter {
+
+    void showToken(String token);
+
+    void showErrorDialog(String message);
 }
--- app/src/main/java/pe/edu/pucp/grupo02/asistenciapucp/features/student/token/IStudentTokenView.java
+++ app/src/main/java/pe/edu/pucp/grupo02/asistenciapucp/features/student/token/IStudentTokenView.java
@@ -3,4 +3,9 @@ package pe.edu.pucp.grupo02.asistenciapucp.features.student.token;
 import pe.edu.pucp.grupo02.asistenciapucp.features.base.IView;
 
 public interface IStudentTokenView extends IView {
+
+    void showTokenStudent(String token);
+
+    void showErrorDialog(String message);
+
 }

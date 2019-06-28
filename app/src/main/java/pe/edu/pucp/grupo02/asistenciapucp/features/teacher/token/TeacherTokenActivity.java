e009eae041700fda07dd7eeef4fa621eb1f5af1f
dispositivos123 <programacion.dispositivos@gmail.com>
2019-06-27 19:14:44 -0500

    token

--- app/src/main/java/pe/edu/pucp/grupo02/asistenciapucp/features/teacher/token/TeacherTokenActivity.java
+++ app/src/main/java/pe/edu/pucp/grupo02/asistenciapucp/features/teacher/token/TeacherTokenActivity.java
@@ -1,9 +1,15 @@
 package pe.edu.pucp.grupo02.asistenciapucp.features.teacher.token;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 import pe.edu.pucp.grupo02.asistenciapucp.R;
+import pe.edu.pucp.grupo02.asistenciapucp.features.student.messages.StudentMessagesActivity;
 
 public class TeacherTokenActivity extends AppCompatActivity {
 
@@ -11,5 +17,10 @@ public class TeacherTokenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_token);
+    }
+
+    public void generarToken(View view){
+        int random = new  Random().nextInt(10000) + 10000;
+        ((TextView) findViewById(R.id.teacherToken_txt_token)).setText(String.valueOf(random));
     }
 }
